7b51fb53ebe9f1c41feee8b1713c371be04aa0e2
Adina Henderson <adinahenderson.mail@gmail.com>
2021-04-23 17:31:00 -0400

    Updated js and html

--- develop/js/script.js
+++ develop/js/script.js
@@ -8,10 +8,11 @@ var formLabel = document.getElementById('#form-label');
 var historyBtn = document.querySelector('.historyBtn');
 var wImage = document.querySelector('.card-img-top');
 var populateHistoryUl = document.querySelector('#populateHistory');
-
+var forecastFilteredData;
+var weatherForecast = {};
+var appKey = '766dfa77a15860a4f2cd465763e36bb6';
 
 // 1. Search data based on user selection
-var appKey = '766dfa77a15860a4f2cd465763e36bb6';
 var displayWeather = function (e) {
     e.preventDefault();
     // use userInput to fecth data in openweathermap.org
@@ -20,34 +21,36 @@ var displayWeather = function (e) {
     // Unhide Current and Forecast Weather sections
     currentSec.setAttribute("style","display:block;");
     fiveDayFSec.setAttribute("style","display:block;");
-
+    // Run fetch
     fetchWeather(userInput);    
 }
 
 // 2. Fetch weather data to populate cards
 function fetchWeather(userInput) {
     var wthrUrl = 'https://api.openweathermap.org/data/2.5/weather?q='+userInput+'&appid='+appKey+'&units=imperial';
-    var forecastFiveDay = 'https://api.openweathermap.org/data/2.5/forecast?q='+userInput+'&appid='+appKey+'&units=imperial';
+    var fiveDayForecast = 'https://api.openweathermap.org/data/2.5/forecast?q='+userInput+'&appid='+appKey+'&units=imperial';
 
     fetch (wthrUrl)
     .then (function(response){
         if(response.ok){
             response.json().then(function(data) {
-                console.log(data);
+                console.log("Line 75 --> Data from first fetch"); // remove when done
+                console.log(data); // remove when done
 
             // Fetch the UV data    
                 fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${data.coord.lat}&lon=${data.coord.lon}&appid=${appKey}`)
                 .then(res => res.json())
                 .then(uvData => {
-                    console.log("List of uvData for 5 day forecast");
-                    console.log(uvData)
+                    console.log("Line 81 --> List of uvData for Current"); // remove when done
+                    console.log(uvData) // remove when done
                     if(uvData.current.uvi < 3){
-                        // color green
+                        // ----> set colors --- green, yellow, red
+                        
                     }
                     populateCard(data,uvData);
+                    dataForecast(userInput, fiveDayForecast);
                 })   
-            }); 
-            
+            });   
         }
         else {
             // type of errors
@@ -58,24 +61,71 @@ function fetchWeather(userInput) {
     .catch (function (error){
         alert("Could not connect to OpenWeather");
     });
-    fetch(forecastFiveDay)
-        .then(res => res.json())
-        .then(forecastData => {
-            console.log("What variables created for 5 day forecast");
-            console.log(forecastData)
-            var filteredDays = forecastData.list.filter(query => query.dt_txt.includes('12:00:00'))
-            console.log(filteredDays)
-// call function for printing the 5 cards
-            console.log("forecastData to use in 5-Day function");
-            populateFive(forecastData);
+    // Function to fetch 5 day Forecast
+    function dataForecast(userInput, fiveDayForecast) {  
+        fetch(fiveDayForecast)
+            .then(res => res.json())
 
-        })
+            .then(forecastData => {
+                console.log("Line-64--> 5 day forecast");
+                console.log(forecastData)
+                var filteredDays = forecastData.list.filter(query => query.dt_txt.includes('12:00:00'));
 
+                // Call to populate 5-day Forecast
+                populateFive(filteredDays, fiveDayFSec);               
+            })      
+    }
 }
+
+// 4. Function to populate 5-day Forecast cards with loop --forecastFilteredData
+function populateFive(filteredDays, fiveDayFSec){
+    console.log(filteredDays); //remove when done
+    for (var i = 0; i < filteredDays.length; i ++) {
+        console.log(filteredDays[i].dt_txt);
+        console.log("Line 158 ---> From Function populateFive ---> Data from filtered from local storage to populate 5-day Forecast:") // delete after test
+        // Add new card in Five Day Forecast Sectioon
+        var newCard = document.createElement('div');
+        newCard.setAttribute("class","card tarjeta");
+        fiveDayFSec.appendChild(newCard);
+        // New Image in card   
+        var newImg = document.createElement('img');
+        newImg.setAttribute("id", "wthIcon");
+        newCard.appendChild(newImg);
+        newImg.setAttribute("class","card-img-top")
+        var wIcon = document.querySelector('#wthIcon');
+        var fPic = filteredDays[i].weather[0].icon;
+        var fImageSrc = 'http://openweathermap.org/img/wn/'+fPic+'@2x.png';
+        wIcon.src = fImageSrc;
+        console.log(wIcon.src); //remove after done
+        // New div to attach Weather info
+        var newDiv = document.createElement("div");
+        newDiv.setAttribute("class", "card cardForecast");
+        newCard.appendChild(newDiv);
+        // New h5 in new div
+        var newh5 = document.createElement("h5");
+        newh5.setAttribute("class", "card-title");
+        newDiv.appendChild(newh5);
+        var newP = document.createElement("p");
+        newP.setAttribute("class","card-text");
+        newDiv.appendChild(newP);
+        // Print data
+        document.querySelector(".cardForecast").textContent = "Date: "+filteredDays[i].dt_txt+"<br>Temperature: "+filteredDays[i].main.temp+" °F"+"<br>Humidity: "+filteredDays[i].main.humidity+" %"+"<br>Wind: "+filteredDays[i].wind.speed+" mph";    
+        
+        /*
+        // Display weather forecast
+        function displayWeatherForecast() {
+            iconElement.innerHTML = wIcon.source;
+            cityElement.innerHTML = userInputEl.value;
+            tempElement.innerHTML = filteredDays[i].main.temp + " °F";
+        }
+        */
+    }
+}  
+    
 
 
 // 3. Populate card with fetched data
-function populateCard(data,uvData){
+function populateCard(data,uvData) {
     // Add icon to the image section
     var wPic = data.weather[0].icon;
     var wImageSrc = 'http://openweathermap.org/img/wn/'+wPic+'@2x.png';
@@ -91,64 +141,21 @@ function populateCard(data,uvData){
     // Print search history
     if(!result){
         printHistory(historySec);
-
     }
     // Unhide Current and Forecast Weather sections
     currentSec.setAttribute("style","display:block;");
     fiveDayFSec.setAttribute("style","display:block;");
 }
 
-// 4. Function to populate 5-day Forecast cards with loop
-function populateFive(forecastData) {
-    console.log("Data from forecastData fetch to populate 5-day Forecast:")
-    console.log(forecastData.list);
-    for (var cards = 0; cards < 5; cards ++) {
-        // Add new card in Five Day Forecast Sectioon
-        var newCard = document.createElement('div');
-        newCard.setAttribute("class","card tarjeta");
-        fiveDayFSec.appendChild(newCard);
-        // New Image in card   
-        var newImg = document.createElement('img');
-        newImg.setAttribute("id", "wthIcon");
-        newCard.appendChild(newImg);
-        newImg.setAttribute("class","card-img-top")
-        var wIcon = document.querySelector('#wthIcon');
-        var fPic = forecastData.list[cards].weather[0].icon;
-        var fImageSrc = 'http://openweathermap.org/img/wn/'+fPic+'@2x.png';
-        wIcon.src = fImageSrc;
-        console.log(wIcon.src);
-        // New div to attach Weather info
-        var newDiv = document.createElement("div");
-        newDiv.setAttribute("class", "card cardForecast");
-        newCard.appendChild(newDiv);
-        // New h5 in new div
-        var newh5 = document.createElement("h5");
-        newh5.setAttribute("class", "card-title");
-        newDiv.appendChild(newh5);
-        var newP = document.createElement("p");
-        newP.setAttribute("class","card-text");
-        newDiv.appendChild(newP);
-        // Print data
-        document.querySelector(".cardForecast").innerHTML = "Date: "+forecastData.list[cards].dt_txt+"<br>Temperature: "+forecastData.list[cards].main.temp+" °F"+"<br>Humidity: "+forecastData.list[cards].main.humidity+" %"+"<br>Wind: "+forecastData.list[cards].wind.speed+" mph";
-        
-        }
-
-
-        // create elements with attribute & class names, and append elements
-    }
-
-
-
-
 // 3. Store City Search History in the stored History Buttons
 function saveToLocal (userInput) {
     // creates array to save and inializes local storage array
     var keyFromSearch = JSON.parse(localStorage.getItem("weatherHistory"))||[];
     // Prevent duplication of cities or buttons
-    var isOnLocal=false;
-    for(var i=0;i<keyFromSearch.length;i++){
+    var isOnLocal = false;
+    for(var i = 0; i < keyFromSearch.length; i ++){
         if(userInput == keyFromSearch[i]){
-            isOnLocal=true;
+            isOnLocal = true;
             break;
         }
     }
@@ -187,20 +194,12 @@ var displayCityHistory = function (e) {
     else {
         userInputEl.value = e.target.dataset.value;
         fetchWeather(e.target.dataset.value);
-        
     }     
 }
-
-
-
-
-
-
 
 // On click search button, create button with city name
 searchBtn.addEventListener('click',displayWeather);
 populateHistoryUl.addEventListener('click', displayCityHistory);
-
 
 // Print Search History
 printHistory(historySec);
